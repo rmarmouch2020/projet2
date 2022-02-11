700fadd8f5e369cfe83aecd7b4241e8599116e3c
rmarmouch2020 <rachid.marmouch@yahoo.fr>
2022-02-11 19:23:53 +0100

    test

--- projet 2.py
+++ projet 2.py
@@ -4,7 +4,7 @@ from math import ceil
 import urllib
 import csv
 import os
-
+# test
 # Dictionnaire de caractères qui ne peuvent pas être utilisés pour les noms de dossiers ou de fichiers
 forbiddencharacters = {60: None, 62: None, 58: None, 8220: None, 47: None, 92: None, 124: None, 63: None, 42: None, 46: None, 39: None, 34: None}
 # Récupérer le chemin du répertoire courant
